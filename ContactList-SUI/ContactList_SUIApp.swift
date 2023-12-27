f9a07320c765e5d20c9c35741740a8fac212341e
Samir Dzhafari <samirjafari2001@icloud.com>
2023-12-27 21:14:18 +0300

    Initial Commit

---
+++ ContactList-SUI/ContactList_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SUIApp.swift
+//  ContactList-SUI
+//
+//  Created by Самир Джафари on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
